2afc664cce6df0a1a83ed07ab3a8b1bbaf843c89
Viktoria <doduchenko29@gmail.com>
2021-06-16 09:57:02 +0300

    add

--- index.js
+++ index.js
@@ -25,10 +25,10 @@
   return `${diffDay}d ${diffHours}h ${diffMin}m ${diffSec}s`;
 };*/
 
-export const getDiff = (startDate, endDate) => {
+/*export const getDiff = (startDate, endDate) => {
   const diff = new Date(new Date(endDate) - new Date(startDate));
   return `${diff.getDate()}d ${diff.getHours()}h ${diff.getMinutes()}m ${diff.getSeconds()}s`;
-};
+};*/
 
 /*const getDiff = (startDate, endDate) => {
   const diff = new Date(startDate.getTime() - endDate.getTime());
@@ -45,9 +45,27 @@ export const getDiff = (startDate, endDate) => {
   return `${a}d ${b}h ${c}m ${d}s`;
 };*/
 
+export const getDiff = (startDate, endDate) => {
+  let one_day = 1000 * 60 * 60 * 24;
+
+  let date1_ms = startDate.getTime();
+  let date2_ms = endDate.getTime();
+
+  let difference_ms = date2_ms - date1_ms;
+
+  difference_ms = difference_ms / 1000;
+  let seconds = Math.floor(difference_ms % 60);
+  difference_ms = difference_ms / 60;
+  let minutes = Math.floor(difference_ms % 60);
+  difference_ms = difference_ms / 60;
+  let hours = Math.floor(difference_ms % 24);
+  let days = Math.floor(difference_ms / 24);
+
+  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+};
 
 //test data
-/*const result = getDiff(
+const result = getDiff(
   new Date(2021, 8, 1, 13, 12, 0),
   new Date(2017, 5, 6, 10, 15, 0)
 );
@@ -55,4 +73,4 @@ export const getDiff = (startDate, endDate) => {
 console.log(result);
 
 const result2 = getDiff(new Date(), new Date('December, 17, 1995, 03:24:00'));
-console.log(result2);*/
+console.log(result2);
